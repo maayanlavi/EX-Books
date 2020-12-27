e6d9a1a68a66fb0dff245df28119b4b005f1e4d2
Moriel Turjeman <moriel99@pop-os.localdomain>
2020-12-27 16:50:35 +0200

    fixed presentation of book object

--- js/main.js
+++ js/main.js
@@ -13,15 +13,46 @@
 // }
 
 
+function getBookData() {    
+    const urlParams = new URLSearchParams(window.location.search);
+    const bookId = urlParams.get('bookId');
+
+    $.ajax({
+        url: `http://openlibrary.org/works/${bookId}.json`,
+        type: 'GET',
+        success: function (res) {
+            $('#bookTitle').text(res.title)
+
+            $.ajax({
+                url: `http://openlibrary.org${res.authors[0].author.key}.json`,
+                type: 'GET',
+                success: function (res) {
+                    $('#authors').text(res.name)
+                }
+            })
+
+            $('#bookCover').attr('src',`http://covers.openlibrary.org/b/id/${res.covers[0]}-M.jpg`)
+
+        }
+    })
+
+    
+
+
+}
+
 function showResults() {
-    var name = document.getElementById("try").value;
+    let name = document.getElementById("try").value;
     //alert(name);
     $.ajax({
         url: `http://openlibrary.org/search.json?title=${name}`,
         type: 'GET',
         //data: user,
-        success: function (name) {
-            alert(name);
+        success: function (res) {
+            if (res.docs.length > 0)
+                window.location.href = `/book.html?bookId=${res.docs[0].key.split('/')[2]}`;
+            else
+                alert("Book not found")
         }
     });
 }
