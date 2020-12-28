436959947a0a0fa3c32587d5f04849981f0a3f30
maayanlavi <maayanlavi95@gmail.com>
2020-12-28 11:51:55 +0200

    crud function in the client side - preparation

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-// $(function() {
-//     getAllBooks();
-// })
-
-// function getAllBooks() {
-//     $.ajax({
-//         url: `http://localhost:3000/api/books`,
-//         type: 'GET',
-//         success: function (match) {
-//             recreateTable(match);
-//         }
-//     });
-// }
-
-
 function getBookData() {    
     const urlParams = new URLSearchParams(window.location.search);
     const bookId = urlParams.get('bookId');
@@ -30,29 +15,49 @@ function getBookData() {
                     $('#authors').text(res.name)
                 }
             })
-
             $('#bookCover').attr('src',`http://covers.openlibrary.org/b/id/${res.covers[0]}-M.jpg`)
-
         }
     })
-
-    
-
 
 }
 
 function showResults() {
     let name = document.getElementById("try").value;
-    //alert(name);
     $.ajax({
         url: `http://openlibrary.org/search.json?title=${name}`,
         type: 'GET',
-        //data: user,
         success: function (res) {
             if (res.docs.length > 0)
-                window.location.href = `/book.html?bookId=${res.docs[0].key.split('/')[2]}`;
+                window.location.href = `book.html?bookId=${res.docs[0].key.split('/')[2]}`;
             else
                 alert("Book not found")
         }
     });
 }
+
+
+
+function addReview() {
+    //TODO - here we need to pass the user ID & book ID to the server and do "POST" to review with him
+    $.ajax({
+    //  url:        link from moriel
+        type: 'POST',
+        success: function (review) {
+            createReview(review);
+        }
+    });
+}
+
+function createReview(review) {
+    // TODO- Here we need that the review will add to "book" page 
+    // and the book will add to "all book" page
+}
+
+
+function addToMyBooks(){
+    //TODO-here we need that the book will add to "user (my) book"
+    alert("Added Successfully!")
+}
+
+
+
